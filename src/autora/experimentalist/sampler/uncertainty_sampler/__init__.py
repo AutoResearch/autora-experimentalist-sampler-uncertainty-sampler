97beb263d6ffe67d1317430b1d23208570ed7769
Chad C Williams <ccwillia@uvic.ca>
2023-05-16 13:40:55 -0700

    Updates sampler location

--- src/autora/experimentalist/sampler/uncertainty_sampler/__init__.py
+++ src/autora/experimentalist/sampler/uncertainty_sampler/__init__.py
@@ -1,35 +1,59 @@
-"""
-Example Experimentalist Sampler
-"""
-
-
+from typing import Iterable
 import numpy as np
-from typing import Optional
+from scipy.stats import entropy
 
-def example_sampler(
-    condition_pool: np.ndarray, num_samples: Optional[int] = None) -> np.ndarray:
+def uncertainty_sampler(X, model, n, measure="least_confident"):
     """
-    Add a description of the sampler here.
 
     Args:
-        condition_pool: pool of experimental conditions to evaluate
-        num_samples: number of experimental conditions to select
+        X: pool of IV conditions to evaluate uncertainty
+        model: Scikit-learn model, must have `predict_proba` method.
+        n: number of samples to select
+        measure: method to evaluate uncertainty. Options:
 
-    Returns:
-        Sampled pool of experimental conditions
+            - `'least_confident'`: $x* = \\operatorname{argmax} \\left( 1-P(\\hat{y}|x) \\right)$,
+              where $\\hat{y} = \\operatorname{argmax} P(y_i|x)$
+            - `'margin'`:
+              $x* = \\operatorname{argmax} \\left( P(\\hat{y}_1|x) - P(\\hat{y}_2|x) \\right)$,
+              where $\\hat{y}_1$ and $\\hat{y}_2$ are the first and second most probable
+              class labels under the model, respectively.
+            - `'entropy'`:
+              $x* = \\operatorname{argmax} \\left( - \\sum P(y_i|x)
+              \\operatorname{log} P(y_i|x) \\right)$
 
-    *Optional*
-    Examples:
-        These examples add documentation and also work as tests
-        >>> example_sampler([1, 2, 3, 4])
-        1
-        >>> example_sampler(range(3, 10))
-        3
+    Returns: Sampled pool
 
     """
-    if num_samples is None:
-        num_samples = condition_pool.shape[0]
 
-    new_conditions = condition_pool
+    if isinstance(X, Iterable):
+        X = np.array(list(X))
 
-    return new_conditions[:num_samples]
+    a_prob = model.predict_proba(X)
+
+    if measure == "least_confident":
+        # Calculate uncertainty of max probability class
+        a_uncertainty = 1 - a_prob.max(axis=1)
+        # Get index of largest uncertainties
+        idx = np.flip(a_uncertainty.argsort()[-n:])
+
+    elif measure == "margin":
+        # Sort values by row descending
+        a_part = np.partition(-a_prob, 1, axis=1)
+        # Calculate difference between 2 largest probabilities
+        a_margin = -a_part[:, 0] + a_part[:, 1]
+        # Determine index of smallest margins
+        idx = a_margin.argsort()[:n]
+
+    elif measure == "entropy":
+        # Calculate entropy
+        a_entropy = entropy(a_prob.T)
+        # Get index of largest entropies
+        idx = np.flip(a_entropy.argsort()[-n:])
+
+    else:
+        raise ValueError(
+            f"Unsupported uncertainty measure: '{measure}'\n"
+            f"Only 'least_confident', 'margin', or 'entropy' is supported."
+        )
+
+    return X[idx]
